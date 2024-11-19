9092aab4dde890ed50807fcb17e69af881e98a41
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-11-18 23:40:29 -0500

    refactor: :recycle: Add AirplaneType parameter to RegisterAirplane task
    Co-authored-by: josegmez <jose.gomez14@udea.edu.co>
    Co-authored-by: JuanPablo-A <jpablo.arango1@udea.edu.co>
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>

--- src/main/java/co/edu/udea/codefactory/gestion_vuelos/tasks/RegisterAirplane.java
+++ src/main/java/co/edu/udea/codefactory/gestion_vuelos/tasks/RegisterAirplane.java
@@ -1,5 +1,6 @@
 package co.edu.udea.codefactory.gestion_vuelos.tasks;
 
+import co.edu.udea.codefactory.gestion_vuelos.models.AirplaneType;
 import co.edu.udea.codefactory.gestion_vuelos.userinterfaces.RegisterAirplaneInterface;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
@@ -9,30 +10,34 @@ import net.serenitybdd.screenplay.actions.Enter;
 
 public class RegisterAirplane implements Task {
 
-    private final String name;
-    private final String family;
-    private final int seatNumber;
-    private final String seatLayout;
+    private final AirplaneType airplaneType;
 
-    public RegisterAirplane(String name, String family, int seatNumber, String seatLayout) {
-        this.name = name;
-        this.family = family;
-        this.seatNumber = seatNumber;
-        this.seatLayout = seatLayout;
+    public RegisterAirplane(AirplaneType airplaneType) {
+        this.airplaneType = airplaneType;
     }
 
     @Override
     public <T extends Actor> void performAs(T actor) {
+        Integer[] seatValues = this.airplaneType.getSeatLayoutParts();
+
         actor.attemptsTo(
-                Enter.theValue(name).into(RegisterAirplaneInterface.AIRPLANE_NAME_FIELD),
-                Enter.theValue(family).into(RegisterAirplaneInterface.FAMILY_FIELD),
-                Enter.theValue(String.valueOf(seatNumber)).into(RegisterAirplaneInterface.SEAT_NUMBER_FIELD),
-                Enter.theValue(seatLayout).into(RegisterAirplaneInterface.SEAT_LAYOUT_FIELDS),
-                Click.on(RegisterAirplaneInterface.SUBMIT_BUTTON)
+                Enter.theValue(airplaneType.getName()).into(RegisterAirplaneInterface.AIRPLANE_NAME_FIELD),
+                Click.on(RegisterAirplaneInterface.FAMILY_FIELD),
+                Click.on(RegisterAirplaneInterface.FAMILY_OPTION.of(airplaneType.getFamily())),
+                Enter.theValue(String.valueOf(airplaneType.getSeatNumber())).into(RegisterAirplaneInterface.SEAT_NUMBER_FIELD),
+                Click.on(RegisterAirplaneInterface.ADD_SEAT_LAYOUT_ROW_BUTTON),
+                Click.on(RegisterAirplaneInterface.ADD_SEAT_LAYOUT_ROW_BUTTON)
         );
+
+        for (int i = 0; i < seatValues.length; i++) {
+            actor.attemptsTo(
+                    Enter.theValue(String.valueOf(seatValues[i]))
+                            .into(RegisterAirplaneInterface.SEAT_LAYOUT_FIELDS.of(String.valueOf(i + 1)))
+            );
+        }
     }
 
-    public static RegisterAirplane withDetails(String name, String family, int seatNumber, String seatLayout) {
-        return Tasks.instrumented(RegisterAirplane.class, name, family, seatNumber, seatLayout);
+    public static RegisterAirplane withDetails(AirplaneType airplaneType) {
+        return Tasks.instrumented(RegisterAirplane.class, airplaneType);
     }
 }
