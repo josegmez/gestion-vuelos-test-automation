8231df3deb019512c006a7a9263f696a3d8a69c8
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-11-18 23:43:16 -0500

    feat: :sparkles: Add steps definition to complete airplane registration feature
    Co-authored-by: josegmez <jose.gomez14@udea.edu.co>
    Co-authored-by: JuanPablo-A <jpablo.arango1@udea.edu.co>
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>

--- src/test/java/co/edu/udea/codefactory/gestion_vuelos/stepdefinitions/AirplaneManagementSteps.java
+++ src/test/java/co/edu/udea/codefactory/gestion_vuelos/stepdefinitions/AirplaneManagementSteps.java
@@ -1,12 +1,26 @@
 package co.edu.udea.codefactory.gestion_vuelos.stepdefinitions;
 
 import co.edu.udea.codefactory.gestion_vuelos.interactions.Click;
+import co.edu.udea.codefactory.gestion_vuelos.models.AirplaneType;
+import co.edu.udea.codefactory.gestion_vuelos.questions.Item;
+import co.edu.udea.codefactory.gestion_vuelos.questions.ToastMessage;
+import co.edu.udea.codefactory.gestion_vuelos.questions.UserLocation;
 import co.edu.udea.codefactory.gestion_vuelos.tasks.Navigate;
+import co.edu.udea.codefactory.gestion_vuelos.tasks.RegisterAirplane;
+import co.edu.udea.codefactory.gestion_vuelos.tasks.SearchAirplane;
 import co.edu.udea.codefactory.gestion_vuelos.userinterfaces.AirplanesInterface;
+import co.edu.udea.codefactory.gestion_vuelos.userinterfaces.RegisterAirplaneInterface;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.targets.Target;
+
+import java.util.Map;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 
 public class AirplaneManagementSteps {
 
@@ -17,8 +31,49 @@ public class AirplaneManagementSteps {
         user.attemptsTo(Navigate.to(new AirplanesInterface(), "/airplane-types"));
     }
 
-    @And("da clic en el botón {string}")
-    public void clickOnAddAirplaneButton(String button) {
+    @And("da clic en el botón Agregar aeronave")
+    public void clickOnAddAirplaneButton() {
         user.attemptsTo(Click.on(AirplanesInterface.ADD_AIRPLANE_BUTTON));
+    }
+
+    @And("es redirigido al formulario para crear un tipo de avión")
+    public void isRedirectedToAirplaneForm() {
+        user.should(seeThat(
+                UserLocation.isAt("/airplane-types/create")
+        ));
+    }
+
+    @When("^ingresa datos válidos en todos los campos requeridos$")
+    public void fillAirplaneForm(Map<String, Object> data) {
+        AirplaneType airplaneType = AirplaneType.FromMap(data);
+        user.attemptsTo(RegisterAirplane.withDetails(airplaneType));
+    }
+    @And("intenta dar clic en el botón {string} dentro del formulario")
+    public void tryClickButtonInsideForm(String arg0) {
+        user.attemptsTo(Click.on(RegisterAirplaneInterface.SUBMIT_BUTTON));
+    }
+
+    @Then("debe mostrar el mensaje de éxito, {string}")
+    public void showSuccesMessage(String message) {
+        user.should(seeThat(ToastMessage.is(message)));
+    }
+
+    @And("redirige hacia la sección de aeronaves")
+    public void isRedirectedToAirplanesSection() {
+        user.should(seeThat(
+                UserLocation.isAt("/airplane-types")
+        ));
+    }
+
+    @And("muestra el tipo de avión {string} registrado en el sistema")
+    public void showRegisteredAirplane(String model) {
+        user.attemptsTo(
+                SearchAirplane.byModel(model)
+        );
+        Target targetRow = Target.the("Airplane Row")
+                .locatedBy("#"+model);
+        user.should(seeThat(
+                Item.isVisible(targetRow)
+        ));
     }
 }
