15aca5aa8b0bff0337d23fe4fa9e4d26065db0f3
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-11-20 00:22:36 -0500

    feat: :sparkles: Add action buttons and dynamic target for airplane rows in AirplanesInterface
    Co-authored-by: GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
    Co-authored-by: JuanPablo-A <jpablo.arango1@udea.edu.co>
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>

--- src/main/java/co/edu/udea/codefactory/gestion_vuelos/userinterfaces/AirplanesInterface.java
+++ src/main/java/co/edu/udea/codefactory/gestion_vuelos/userinterfaces/AirplanesInterface.java
@@ -7,6 +7,17 @@ public class AirplanesInterface extends PageObject {
     public static final Target ADD_AIRPLANE_BUTTON = Target.the("Agregar aeronave button")
             .locatedBy("#add-airplane-type");
 
+    public static final Target ACTIONS_BUTTON = Target.the("Boton acciones")
+            .locatedBy("#actions>div>button:first-of-type");
+
+    public static final Target EDIT_ACTION_BUTTON = Target.the("Editar aeronave button")
+            .locatedBy("#edit");
+
     public static final Target FILTER_BY_MODEL_INPUT = Target.the("Filtrar por modelo")
             .locatedBy("#filter-airplane-type-model");
+
+    public static Target airplaneRow(String model) {
+        return Target.the("Airplane Row")
+                .locatedBy("#" + model);
+    }
 }
