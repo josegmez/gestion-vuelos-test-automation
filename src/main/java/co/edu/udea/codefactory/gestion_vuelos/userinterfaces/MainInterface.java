158e27e5ef105fb3b8cd9d766b7be07c09526fde
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-11-20 15:19:23 -0500

    feat: :sparkles: Add MainInterface class for the main user interface

---
+++ src/main/java/co/edu/udea/codefactory/gestion_vuelos/userinterfaces/MainInterface.java
@@ -0,0 +1,9 @@
+package co.edu.udea.codefactory.gestion_vuelos.userinterfaces;
+
+import net.serenitybdd.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+
+@DefaultUrl("/")
+public class MainInterface extends PageObject {
+
+}
